c194d7808906802e7eb565473276eab3f7db2a8b
Lucas Gelinas <lucas.gelinas@ocsbstudent.ca>
2021-02-17 17:30:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let a = 0
+let b = 0
+let h = 0 
+let total = 0
+
+document.getElementById('button').addEventListener('click',addition)
+
+function addition () {
+  a = document.getElementById('a').value
+
+  a = parseInt(a)
+
+  b = document.getElementById('b').value
+
+  b = parseInt(b)
+
+  h = document.getElementById('h').value
+
+  h = parseInt(h)
+
+  total=((a + b) / 2) * h
+
+  alert(total)
+}
